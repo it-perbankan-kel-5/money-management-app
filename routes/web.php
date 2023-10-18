7c0d4ca37792eed4baa43d09d0a45a8ed54050a2
MrLuluCat <thenabil11@gmail.com>
2023-10-18 14:42:15 +0700

    Editing Template Engine

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('components.layout');
 });
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
