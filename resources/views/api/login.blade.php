212faf9925c8423e7095749a3927b3da99c85f8e
MrLuluCat <thenabil11@gmail.com>
2023-10-21 22:48:54 +0700

    update login

--- resources/views/api/login.blade.php
+++ resources/views/api/login.blade.php
@@ -6,8 +6,9 @@
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>Rakmain Banking</title>
-  <link rel="shortcut icon" href="{{ asset("assets/images/rakamin.png") }}" />
+  <title>Rakamin Banking</title>
+
+  <link rel="shortcut icon" href="{{ asset("assets/images/logo.png") }}" />
   
   <!-- plugins:css -->
   <link rel="stylesheet" href="{{ asset("assets/vendors/feather/feather.css") }}">
@@ -27,67 +28,80 @@
 </head>
 
 <body>
+
 <div class="container-scroller">
   <div class="container-fluid page-body-wrapper full-page-wrapper">
     <div class="content-wrapper d-flex align-items-stretch auth auth-img-bg">
       <div class="row flex-grow">
-          <div class="col-lg-6 login-half-bg d-flex flex-row">
-          </div>
-          <div class="col-lg-6 d-flex align-items-center justify-content-center">
-            <div class="auth-form-transparent text-left p-3">
-              <div class="brand-logo">
-                <img src="../../../assets/images/logo.png" alt="logo">
-              </div>
-              <h3 class="text1">Welcome to FlexiFunds Apps</h3>
-              <h5 class="font-weight-light">Happy to see you again!</h5>
-              <form class="pt-3">
-                <div class="form-group">
-                  <label for="exampleInputEmail">Username</label>
-                  <div class="input-group">
-                    <div class="input-group-prepend bg-transparent">
-                      <span class="input-group-text bg-transparent border-right-0">
-                        <i class="ti-user text-primary"></i>
-                      </span>
-                    </div>
-                    <input type="text" class="form-control form-control-lg border-left-0" id="exampleInputEmail" placeholder="Username">
-                  </div>
-                </div>
-                <div class="form-group">
-                  <label for="exampleInputPassword">Password</label>
-                  <div class="input-group">
-                    <div class="input-group-prepend bg-transparent">
-                      <span class="input-group-text bg-transparent border-right-0">
-                        <i class="ti-lock text-primary"></i>
-                      </span>
-                    </div>
-                    <input type="password" class="form-control form-control-lg border-left-0" id="exampleInputPassword" placeholder="Password">                        
-                  </div>
-                </div>
-                <div class="my-2 d-flex justify-content-between align-items-center">
-                  <div class="form-check">
-                    <label class="form-check-label text-muted">
-                      <input type="checkbox" class="form-check-input">
-                      Keep me signed in
-                    <i class="input-helper"></i></label>
-                  </div>
-                  <a href="#" class="auth-link text-danger">Forgot password?</a>
-                </div>
-                <div class="my-3">
-                  <a class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn" href="../../index.html">LOGIN</a>
-                </div>
-                <div class="text-center mt-4 font-weight-light">
-                  Don't have an account? <a href="register-2.html" class="text-primary">Create</a>
-                </div>
-              </form>
+        <div class="col-lg-6 d-flex flex-row scale">
+          <!-- Add content for the left half here -->
+          <img src="{{ asset("assets/images/logo-login.png") }}" alt="logo">
+        </div>
+        <div class="col-lg-6 d-flex align-items-center justify-content-center">
+          <div class="auth-form-transparent text-left p-3">
+            <div class="brand-logo">
+              <img src="{{ asset("assets/images/logo.png") }}" alt="logo" class="img-fluid">
             </div>
+            <h3 class="text1">Welcome to FlexiFunds Apps</h3>
+            <h5 class="font-weight-light">Happy to see you again!</h5>
+
+            <form method="POST" action="" class="pt-3">
+              <div class="form-group">
+                <label for="exampleInputEmail">Username</label>
+                <div class="input-group">
+                  <div class="input-group-prepend bg-transparent">
+                    <span class="input-group-text bg-transparent border-right-0">
+                      <i class="ti-user text-primary"></i>
+                    </span>
+                  </div>
+                  <input type="text" class="form-control form-control-lg border-left-0" id="exampleInputEmail" placeholder="Username" required>
+                </div>
+              </div>
+
+              <div class="form-group">
+                <label for="exampleInputPassword">Password</label>
+                <div class="input-group">
+                  <div class="input-group-prepend bg-transparent">
+                    <span class="input-group-text bg-transparent border-right-0">
+                      <i class="ti-lock text-primary"></i>
+                    </span>
+                  </div>
+                  <input type="password" class="form-control form-control-lg border-left-0" id="exampleInputPassword" placeholder="Password" required>
+                </div>
+              </div>
+
+              <div class="my-2 d-flex justify-content-between align-items-center">
+                <div class="form-check">
+                  <label class="form-check-label text-muted">
+                    <input type="checkbox" class="form-check-input">
+                    Keep me signed in
+                    <i class="input-helper"></i>
+                  </label>
+                </div>
+                <a href="#" class="auth-link text-danger">Forgot password?</a>
+              </div>
+
+              <div class="my-3">
+                <a class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn" href="../../index.html">LOGIN</a>
+              </div>
+
+              <div class="text-center mt-4 font-weight-light">
+                Don't have an account? <a href="/register" class="text-primary">Create</a>
+              </div>
+            </form>
+
           </div>
         </div>
-        <!-- content-wrapper ends -->
       </div>
-      <!-- container-fluid ends -->
+      <!-- content-wrapper ends -->
     </div>
-    <!-- container-scroller ends -->
+    <!-- container-fluid ends -->
+  </div>
+  <!-- container-scroller ends -->
 </div>
+
+</body>
+
       
   <!-- plugins:js -->
   <script src="{{ asset("assets/vendors/js/vendor.bundle.base.js") }} "></script>
