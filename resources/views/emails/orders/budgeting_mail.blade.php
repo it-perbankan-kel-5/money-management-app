1dc9a5bad5d2320a7d24a27cdcbe81de94a1a510
mulyanan26 <mulyanan.ur26@gmail.com>
2023-11-07 06:30:38 +0700

    Fix bug budgeting mail

--- resources/views/emails/orders/budgeting_mail.blade.php
+++ resources/views/emails/orders/budgeting_mail.blade.php
@@ -14,16 +14,17 @@ This is the panel content.
     </table>
     <table>
         <tr>
-          <td align="left" style="font-size: 20px"><p style="font-size: 20px">Your <b>Budget Eat & Drink<b> has reached 65% of the limit. <br> Rp 300.000-, Remaining.</p></td>
-          <td align="right">
-            <div role="progressbar" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100" style="--value:65">65%</div>
+          <td align="left" style="font-size: 20px"><p style="font-size: 20px">Your <b>Budget Eat & Drink</b> has reached 65% of the limit. <br> Rp 300.000-, Remaining.</p></td>
+          <td>
+            <div class="progress-radial progress-65">
+              <div class="overlay">65%</div>
+            </div>
           </td>
         </tr>
       </table>
 
       
 </x-mail::table>
-
 
 
 Thanks,<br>
